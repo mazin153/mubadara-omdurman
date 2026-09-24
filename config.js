7249cf0268dc1fa94393829ec81416157df27dd3
mazin153 <maznmhmed9@gmail.com>
2026-09-24 09:18:24 +0200

    Change Supabase_ANON_KEY in config.js
    Updated Supabase anonymous key for configuration.

--- config.js
+++ config.js
@@ -8,7 +8,7 @@
 
 // إعدادات Supabase
 const SUPABASE_URL = "https://fkojmjghrqzmcapfdicz.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrb2ptamd..."; // المفتاح الطويل كاملاً هنا
+const SUPABASE_ANON_KEY = "sb_publishable_2WEGGksGAuJ-Y61L4UrcVw_lhMwusm7"; // المفتاح الطويل كاملاً هنا
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
